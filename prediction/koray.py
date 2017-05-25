ec01642ab98b94abacc2f8d54c8d4f6cff2aa7a2
Koray <Koray@BKorayOZ>
2017-05-25 13:54:50 +0300

    koray silindi

--- prediction/koray.py
+++
@@ -1,9 +0,0 @@
-from pandas import Series
-from django.http import HttpResponse
-from matplotlib import python
-
-def koray(request):
-    series = Series.from_csv("example.csv", header = 0)
-    series.plot()
-    
-    return HttpResponse(str(series))

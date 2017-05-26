a6d897462348b14023012381394dc8d723941df8
Koray <Koray@BKorayOZ>
2017-05-26 17:06:54 +0300

    prediction model fonksiyonu tamamlandi

--- prediction/koray.py
+++ prediction/koray.py
@@ -9,43 +9,58 @@ import xlrd
 from sklearn.metrics import mean_squared_error
 from numpy.linalg import  LinAlgError
 
-def koray(request):
-    #fields = ['Gun', 'SatisMiktari']
-    #df = pd.read_csv("example.csv", usecols = fields)
-
-    xl = pd.ExcelFile("aylik.xlsx")
-    #xl.sheet_names = [u'Alinan Veriler', u'Haftelik', u'Aylik']
-    df = xl.parse("Sayfa1", header=0, index_col= 0, parse_cols=[0, 1], converters={'a':float}, squeeze=True)
-    pprint(df.head(25))
-    #df.plot()
-    #pyplot.show()
-    data = df
-    pprint("DATA")
-    pprint(data)
-    pprint("SONUC")
-    X = data.values
-    #size = int(len(X) * 0.66)
-    #train, test = X[0:size], X[size:len(X)]
-    #history = [X for x in train]
-    #predictions = list()
-    train, test = X[1:len(X)-2], X[len(X)-2:]
-    model = AR(train)
-    model_fit = model.fit()
-    predictions = model_fit.predict(start = len(train), end = len(train)+len(test)-1, dynamic = False)
+#def koray(request):
+def koray():
+    xl = pd.ExcelFile("temp.xlsx")
+    adata = xl.parse("Sayfa1", header=0, parse_cols=[0, 1], index_col= 0, converters={'a':float}, squeeze=True)
+    bdata = xl.parse("Sayfa1", header=0, parse_cols=[0, 2], index_col= 0, converters={0:float}, skiprows = 51, squeeze=True)
     
-    for i in range(len(predictions)):
-    #for i in range(len(test)):
-        #fitting
-        #model  = ARIMA(data, order = (15, 2, 0))
-        #model_fit = model.fit(disp = 0) # disp = 0, debug info gostermiyor
-        #pprint(model_fit.summary())
-        #plot
-        #outcome = model_fit.forecast()
-        pprint("SONUC")
-        #pprint(outcome[0])
-        pprint(predictions[i])
+    pprint('adata')
+    pprint(adata)
+    pprint("bdata")
+    pprint(bdata)
+
+    predictions = list()
+    for i in range(0, 2):
+        if i == 0:
+            data = adata
+        elif i == 1:
+            data == bdata
+            
+        pprint("DATA")
+        pprint(data)
+        X = data.values
+        size = int(len(X) * 0.66)
+        train, test = X[0:size], X[size:len(X)]
+
+        #train, test = X[1:len(X)-5], X[len(X)-5:]
+        #model = AR(train)
+        #model_fit = model.fit()
+        #predictions = model_fit.predict(start = len(train), end = len(train)+len(test)-1, dynamic = False)
+        
+        #for i in range(len(predictions)):
+        for j in range(0, 12):
+        #for i in range(len(test)):
+            #data.drop(data.count)
+            #count = data.count()
+            #data = data.drop(data.index[count-1])
+            #fitting
+            model  = ARIMA(data, order = (5, 1, 0))
+            model_fit = model.fit(disp = 0) # disp = 0, debug info gostermiyor
+            #pprint(model_fit.summary())
+            #plot
+            outcome = model_fit.forecast()
+            predictions.append(outcome[0])
+            count = data.count()
+            data = data.drop(data.index[count-1])
+
+        pprint("Sonuc")
+        for j in range(0, len(predictions)):
+            pprint(predictions[j])
+        #pprint(predictions[i] * predictions[i])
     #model = AR(df)
     #model_fit = model.fit()
     #prediction = model_fit.predict(start = len(dataset, end = len(dataset)))
     #pprint(str(prediction))
-    return HttpResponse("OPTUM CANIM")
+    return predictions
+    #return HttpResponse("OPTUM CANIM")
